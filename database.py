6b940595c65ee74e540041db0425bca156b0108f
jaennil <jaennil@yandex.ru>
2024-04-18 02:11:34 +0300

    feat: database class

---
+++ database.py
@@ -0,0 +1,43 @@
+import psycopg2
+
+db_params = {
+    "host": "localhost",
+    "database": "keylogger",
+    "user": "jaennil",
+    "password": "naen",
+    "port": "5432"
+}
+
+
+class Database:
+
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+            cls._instance._connection = None
+        return cls._instance
+
+    def connect(self, config=db_params):
+        try:
+            self._connection = psycopg2.connect(**config)
+        except Exception as e:
+            print(f'error occured while connecting to database: {e}')
+
+    def execute(self, query, params=None):
+        if self._connection is None:
+            print("error occured while executing query: database connection is none")
+            return
+
+        cursor = self._connection.cursor()
+        cursor.execute(query, params)
+        result = cursor.fetchall()
+        cursor.close()
+
+        return result
+
+    def disconnect(self):
+        if self._connection:
+            self._connection.close()
+            self._connection = None
