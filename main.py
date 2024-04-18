dd0b073bcd3ba644cf08eea56b1ad44b2cf3c0ea
jaennil <jaennil@yandex.ru>
2024-04-18 13:44:23 +0300

    feat: create session

--- main.py
+++ main.py
@@ -4,17 +4,28 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 
 from editor import EditorWidget
 
+import database
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.session_id = None
 
         self.setCentralWidget(EditorWidget())
         self.initWindow()
 
+        self.create_session()
+
     def initWindow(self):
         self.showMaximized()
         self.setWindowTitle('Code Analyzer')
+
+    def create_session(self):
+        db = database.Database()
+        db.connect()
+        self.session_id = db.execute(
+            "INSERT INTO Sessions(start_time) VALUES(NOW()) RETURNING session_id")
 
 
 if __name__ == "__main__":
